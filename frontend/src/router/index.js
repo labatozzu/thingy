b389249a0df558a66fe46fdbd98fa1e391fbad69
Mikko Niska <mikko.niska@protonmail.com>
2026-02-16 13:42:45 +0200

    Lazy-load route components to reduce initial bundle size

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,11 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { user, fetchMe } from '../composables/useAuth'
-import HomeView from '../views/HomeView.vue'
-import LoginView from '../views/LoginView.vue'
 
 const routes = [
-  { path: '/', name: 'home', component: HomeView },
-  { path: '/login', name: 'login', component: LoginView },
+  { path: '/', name: 'home', component: () => import('../views/HomeView.vue') },
+  { path: '/login', name: 'login', component: () => import('../views/LoginView.vue') },
 ]
 
 export const router = createRouter({
